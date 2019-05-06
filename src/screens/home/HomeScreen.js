d9a2c993a936c0d68754ac09407791f8d2a29d55
Zeuxis Lo <seekstudio@gmail.com>
2019-05-06 18:20:10 +0800

    rename component/navbar to component/header

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { HeaderMaterialButtons, HeaderMaterialItem } from '../../components/navbar';
+import { HeaderMaterialButtons, HeaderMaterialItem } from '../../components/header';
 
 class HomeScreen extends React.Component {
     static navigationOptions = ({ navigation }) => {
