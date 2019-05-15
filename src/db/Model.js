f9b3ccf8d79ba2a666c09615bd8af02fdeeaaf53
Zeuxis Lo <seekstudio@gmail.com>
2019-05-15 20:05:10 +0800

    migrate the model object to new query builder

--- src/db/Model.js
+++ src/db/Model.js
@@ -1,59 +1,18 @@
-import _ from 'lodash';
+import Builder from './Builder';
 
-import DBHelper from './DBHelper';
-
-export default class Model {
-
-    static primaryKey = "id";
-
-    static incrementing = true;
-
-    static schema = {};
-
-    // Get the primary id
-    static id() {
-        const currentId = DBHelper.connection().objects(this.schema.name).max("id");
-
-        return nextId = (_.isUndefined(currentId) || _.isNull(currentId)) ? 1 : currentId + 1;
-    }
-
-    // Create single item
-    static create(item) {
-        return new Promise((resolve, reject) => {
-            try {
-                if (this.incrementing === true) {
-                    item[this.primaryKey] = this.id();
-                }
-
-                DBHelper.connection().write(() => {
-                    const obj = DBHelper.connection().create(this.schema.name, item);
-
-                    resolve(obj);
-                });
-            }catch(e) {
-                reject(e);
-            }
-        });
-    }
-
-    // Get all objects
-    static objects() {
-        try {
-            return DBHelper.connection().objects(this.schema.name);
-        }catch(e) {
-            throw e;
-        }
-    }
-
-    // Get all objects by desc order and convert to array
-    static all() {
-        return new Promise((resolve, reject) => {
-            try {
-                resolve(Array.from(this.objects().sorted("id", true)));
-            }catch(e) {
-                reject(e);
-            }
-        });
-    }
-
+class Model {
 }
+
+// Using proxy to build up missing method for Model
+// like __call in php, method_missing in ruby
+export default new Proxy(Model, {
+    get(object, property) {
+        if (Reflect.has(object, property) === true) {
+            return object[property];
+        }
+
+        let builder = new Builder();
+
+        return builder[property];
+    }
+});
