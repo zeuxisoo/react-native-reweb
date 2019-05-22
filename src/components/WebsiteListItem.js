766857a75b6a13b5cb0c12fa08f729d1d3436e88
Zeuxis Lo <seekstudio@gmail.com>
2019-05-22 16:13:39 +0800

    add prop types to website list item component

--- src/components/WebsiteListItem.js
+++ src/components/WebsiteListItem.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+import { Text } from 'react-native';
 import { Card } from 'react-native-elements';
 import { TouchableButton } from './TouchableButton';
 
 class WebsiteListItem extends React.PureComponent {
+
+    static propTypes = {
+        website: PropTypes.object,
+        onPress: PropTypes.func,
+    }
+
+    static defaultProps = {
+        website: {
+            name: "",
+            url : "",
+        }
+    }
 
     render() {
         return (
