d2a9dc99294e22b7b43866a91d8ad9d37b0fdba0
Zeuxis Lo <seekstudio@gmail.com>
2019-05-24 17:25:22 +0800

    add trash property to websites schema

--- src/db/schemas/WebsiteSchema.js
+++ src/db/schemas/WebsiteSchema.js
@@ -2,8 +2,9 @@ export default WebsiteSchema = {
     name: "websites",
     primaryKey: "id",
     properties: {
-        id  : { type: "int", indexed: true },
-        name: { type: "string" },
-        url : { type: "string" },
+        id   : { type: "int", indexed: true },
+        name : { type: "string" },
+        url  : { type: "string" },
+        trash: { type: "bool", default: false },
     }
 }
